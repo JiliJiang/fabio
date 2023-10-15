1747e98c92c8a7a874fc6d0e93ebd536c1a94867
JiliJiang <jiangjili@gmail.com>
2023-10-14 17:46:15 -0700

    code worked during tutoring session

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize')
+require('dotenv').config();
+
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    'post_db',
+    'Jili',
+    'root',
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+module.exports = sequelize;
---
+++ public/js/new-comment.js
@@ -0,0 +1,32 @@
+const theClassicBlogCommentFormHandler = async (event) => {
+    event.preventDefault(); 
+  
+    const post_id = window.location.pathname.split('/').pop();
+
+    console.log("post_id", post_id);
+  
+    const comment_text = document.querySelector('#text').value.trim();
+  
+    if (content) {
+      const response = await fetch(`/api/comments`, {
+        method: 'POST',
+        body: JSON.stringify({ comment_text, post_id }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.reload(); // When successful, reload the same page
+      } else {
+        // console.log('Response status:', response.status);
+        // console.log('Response text:', await response.text());
+        alert('Failed to create a comment.'); // When unsuccessful, show alert
+      }
+    }
+  };
+  
+  
+  // Event listeners
+  const theClassicBlogCommentForm = document.querySelector('.new-comment-form');
+  
+theClassicBlogCommentForm.addEventListener('submit', theClassicBlogCommentFormHandler);
+  
